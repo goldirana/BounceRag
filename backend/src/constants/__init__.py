406c1d1203150c769a21a4e667c4d7505f12c9bb
goldirana <goldirana3210@gmail.com>
2025-02-08 17:06:50 +0530

    added params

--- backend/src/constants/__init__.py
+++ backend/src/constants/__init__.py
@@ -2,3 +2,4 @@ from pathlib import Path
 
 CONFIG_FILE_PATH = str(Path("config.yaml"))
 PARAMS_FILE_PATH = str(Path("params.yaml"))
+PROMPT_FILE_PATH = str(Path("backend/src/prompts/summarizer.json"))
