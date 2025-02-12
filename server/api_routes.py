fb97cf13f93776b5703da55423612ad11b9346b0
goldirana <goldirana3210@gmail.com>
2025-02-12 16:34:40 +0530

    updated api_routes

--- server/api_routes.py
+++ server/api_routes.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 from server.dependencies import (get_vector_db, get_text_summarizer, 
                                  get_image_summarizer, 
                                  get_rag_service, get_query_service,
-                                 get_llm_model)
-from pydantic import BaseModel
-
+                                 get_llm_model, 
+                                 get_firestore_chat_client)
 
 router = APIRouter()
 
+firestore_chat_history = get_firestore_chat_client()
 rag_service = get_rag_service()
 query_service = get_query_service()
 model = get_llm_model()
@@ -22,4 +23,6 @@ async def search_vb(request: QueryRequest):
     query = request.question
     chain = rag_service.get_chain(query, query_service, model)
     response = await chain.ainvoke(query)
+    firestore_chat_history.add_user_message(query)
+    firestore_chat_history.add_ai_message(response)
     return {"response": response}
