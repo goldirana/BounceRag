47d229b287456913a12ac2de44b868e3acc89dd0
goldirana <goldirana3210@gmail.com>
2025-02-14 19:26:21 +0530

    removed unwanted function

--- server/services/query_service.py
+++ server/services/query_service.py
@@ -24,7 +24,7 @@ class QueryService:
         __init__(self, embeddings, vb: VectorDatabase):
             Initializes the QueryService with embeddings and a vector database.
         search_similar_documents(self, query: str, top_k: int = 5) -> List[Document]:
-        rag_pipeline(self: object, prompt_func, model):
+        # rag_pipeline(self: object, prompt_func, model):
         get_stored_docs(self, retrieved_docs: List[Document]) -> dict:
     """
     def __init__(self, embeddings, vb: VectorDatabase):
@@ -50,26 +50,6 @@ class QueryService:
         results = self.query_handler.similarity_search(query, top_k)
         return results
 
-    def rag_pipeline(self: object, prompt_func, model):
-        """
-        Constructs and returns a processing chain for the RAG (Retrieval-Augmented Generation) pipeline.
-        Args:
-            self (object): The instance of the class containing this method.
-            prompt_func (callable): A function to generate prompts for the model.
-            model (object): The model to be used in the pipeline.
-        Returns:
-            chain: A processing chain 
-            that includes context retrieval, prompt generation, model inference, and output parsing.
-        """
-        
-        chain = (
-            {"context": self.vectorstore_as_retriever | RunnableLambda(QueryHandler.split_image_text_types), 
-             "question": RunnablePassthrough()}
-            | RunnableLambda(prompt_func)
-            | model
-            | StrOutputParser()
-        )
-        return chain
         
     def get_stored_docs(self, retrieved_docs: List[Document])-> dict:
         """
@@ -81,3 +61,25 @@ class QueryService:
         """
         data = self.query_handler.split_image_text_types(retrieved_docs)
         return data
+    
+    
+    # def rag_pipeline(self: object, prompt_func, model):
+    #     """
+    #     Constructs and returns a processing chain for the RAG (Retrieval-Augmented Generation) pipeline.
+    #     Args:
+    #         self (object): The instance of the class containing this method.
+    #         prompt_func (callable): A function to generate prompts for the model.
+    #         model (object): The model to be used in the pipeline.
+    #     Returns:
+    #         chain: A processing chain 
+    #         that includes context retrieval, prompt generation, model inference, and output parsing.
+    #     """
+        
+    #     chain = (
+    #         {"context": self.vectorstore_as_retriever | RunnableLambda(QueryHandler.split_image_text_types), 
+    #          "question": RunnablePassthrough()}
+    #         | RunnableLambda(prompt_func)
+    #         | model
+    #         | StrOutputParser()
+    #     )
+    #     return chain
