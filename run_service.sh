ae41a5e999dafd21481793c4ac00e3022e73e3e8
goldirana <goldirana3210@gmail.com>
2025-02-15 15:38:41 +0530

    removed unwanted folder and files

--- run_service.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Start FastAPI in the background
-uvicorn server.main:app --host 0.0.0.0 --port 8501 &
-
-# Start Streamlit in the foreground
-streamlit run frontend/main.py --server.port=8502
