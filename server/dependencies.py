872f5579cbcc81cae5238a586e5af1b14ca17e97
goldirana <goldirana3210@gmail.com>
2025-02-15 17:28:29 +0530

    depen update to lighten server app

--- server/dependencies.py
+++ server/dependencies.py
@@ -1,9 +1,9 @@
 # call model and vector database here
 from backend.src.storage.chroma_storage import VectorDatabase
-from backend.src.extractors.text_summarizer import TextSummarizer
-from backend.src.extractors.image_summarizer import ImageSummarizer
-from backend.src.extractors.data_ingestion import DataIngestion
-from backend.src.extractors.extract import Extractor
+# from backend.src.extractors.text_summarizer import TextSummarizer
+# from backend.src.extractors.image_summarizer import ImageSummarizer
+# from backend.src.extractors.data_ingestion import DataIngestion
+# from backend.src.extractors.extract import Extractor
 from backend.src.entity.config_entity import *
 from backend.src.utils.common import read_yaml
 from backend.src.storage.firebase_storage import FireStore
@@ -20,8 +20,8 @@ import os
 config_manager = ConfigurationManager(CONFIG_FILE_PATH, PARAMS_FILE_PATH)
 vector_database_config = config_manager.get_vectordatabase_config()
 data_ingestion_config = config_manager.get_data_ingestion_params()
-image_summarizer_config = config_manager.get_image_summarizer_params()
-text_summarizer_config = config_manager.get_text_summarizer_params()
+# image_summarizer_config = config_manager.get_image_summarizer_params()
+# text_summarizer_config = config_manager.get_text_summarizer_params()
 firestore_params = config_manager.get_firebase_params()
 
 
@@ -38,9 +38,9 @@ def get_vector_db(config: VectorDatabaseConfig=vector_database_config) -> object
     vector_database = VectorDatabase(config)
     return vector_database
 
-def get_data_ingestion(config: DataIngestionConfig=data_ingestion_config) -> object:
-    data_ingestion = DataIngestion(config)
-    return data_ingestion
+# def get_data_ingestion(config: DataIngestionConfig=data_ingestion_config) -> object:
+#     data_ingestion = DataIngestion(config)
+#     return data_ingestion
 
 # def get_text_summarizer(config: TextSummarizerConfig=text_summarizer_config) -> object:
 #     text_summarizer = TextSummarizer(config, get_llm_model())
