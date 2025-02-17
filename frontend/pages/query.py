8214dfe05bebe56ecb0622b717bd2f081ce5b192
goldirana <goldirana3210@gmail.com>
2025-02-17 15:52:33 +0530

    testing llama

--- frontend/pages/query.py
+++ frontend/pages/query.py
@@ -22,8 +22,8 @@ def display_metadata(metadata_list):
         st.table(metadata_list)
         
 
-# FAST_API_URL = "http://0.0.0.0:5002"
-FAST_API_URL = os.getenv("FAST_API_URL", "https://server-bnxb.onrender.com")
+FAST_API_URL = "http://0.0.0.0:5004"
+# FAST_API_URL = os.getenv("FAST_API_URL", "https://server-bnxb.onrender.com")
 icons = {
     "user": "👤",  # Add user avatar
     "assistant": "frontend/static/ai.gif"
