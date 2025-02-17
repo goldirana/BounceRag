8f7a199d34e5ed064792f106257b15e3278d1539
goldirana <goldirana3210@gmail.com>
2025-02-17 22:13:18 +0530

    updated cosine

--- server/services/cosine_score.py
+++ server/services/cosine_score.py
@@ -1,19 +1,20 @@
 from sklearn.metrics.pairwise import cosine_similarity
 from backend.src.llm_models import (get_openai_embeddings)
 import numpy as np
+import torch.nn.functional as F
+import torch
 
-def get_top_matching_documents(documents, query, top_n=2):
+def get_top_matching_documents(documents, query, top_n=5):
     """
     Finds the top N documents with the highest cosine similarity to the query.
 
     Args:
-        model: The model to use for embeddings.
         query (str): The query to compare against documents.
-        documents (list of dict): Each document should be a dict with 'content' and 'metadata'.
+        documents (list of langchain.documents.Document): List of langchain documents.
         top_n (int): The number of top documents to return.
 
     Returns:
-        list of dict: The top N documents based on the highest similarity scores.
+        list of langchain.documents.Document: The top N documents based on the highest similarity scores.
     """
     if not documents:
         return []  # Handle empty document list
@@ -27,8 +28,12 @@ def get_top_matching_documents(documents, query, top_n=2):
 
     similarities = cosine_similarity([query_embedding], doc_embeddings)[0]
 
-    # Get indices of the top N highest similarity scores
-    top_indices = np.argsort(similarities)[-top_n:][::-1]
+    # Sort documents by descending similarity scores
+    sorted_docs = [doc for _, doc in sorted(zip(similarities, documents), 
+                                            key=lambda pair: pair[0], 
+                                            reverse=True)]
+    
+    # Return the top N documents
+    return sorted_docs[:top_n]
 
-    top_docs = [documents[i] for i in top_indices]    
-    return top_docs
+
